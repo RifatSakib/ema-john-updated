5de5face893428ab4508d1115a0bdd07085166ff
Rifat Sakib <sakibrifat353@gmail.com>
2023-08-08 23:59:59 +0600

    router added

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -3,6 +3,7 @@ import fakeData from '../../fakeData'
 import './Shop.css'
 import Product from '../Product/Product';
 import Cart from '../Cart/Cart';
+import { addToDatabaseCart } from '../../utilities/databaseManager';
 
 const Shop = () => {
 const first10 = fakeData.slice(0,10);
@@ -13,6 +14,9 @@ const handleAddProduct = (product) =>{
     console.log("product Added!!",product)
     const newCart = [...cart, product];
     setCart(newCart);
+    const sameProduct = newCart.filter(pd=>pd.key === product.key)
+    const count = sameProduct.length;
+    addToDatabaseCart(product.key,count);
 }
 
     return (
@@ -20,7 +24,7 @@ const handleAddProduct = (product) =>{
             <div className="product-container">
 
                 {
-                    products.map(pd => <Product  showAddToCart={true} handleAddProduct = {handleAddProduct} product = {pd}> </Product>)
+                    products.map(pd => <Product  key= {pd.key} showAddToCart={true} handleAddProduct = {handleAddProduct} product = {pd}> </Product>)
                 }
 
             </div>
